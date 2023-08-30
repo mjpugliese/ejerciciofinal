c7f2bf5c7c1396d133a009774ef0220782d71ad2
mjpugliese <maxi.pugliese@gmail.com>
2023-08-30 18:12:50 -0300

    fix: mongoose

--- server.js
+++ server.js
@@ -4,13 +4,12 @@ const routes = require('./routes')
 const app = express()
 const port = 5000
 const url = 'mongodb+srv://maxipugliese:ay3YRmIVEAzqnuAx@cluster0.izvwhuw.mongodb.net/'
-
+mongoose.set('strictQuery', false)
 const path = require('path');
 
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
-
 
 app.use('/registro', routes)
 
